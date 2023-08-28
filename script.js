5b0f1ad1ea30782889a2fc1e8e9a229d53b17c22
K4RU3 <pzKARUzq2@gmail.com>
2023-08-28 22:37:49 +0900

    関数作成

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 const canvas = document.querySelector("#canvas");
-
-const line = (x1, y1, x2, y2) => {
-    canvas.appendChild(obj("line", x1, y1, x2, y2));
-};
-
-const obj = (name, ...p) => {
-    let o = document.createElementNS("http://www.w3.org/2000/svg", name);
-    
-    if (name === "line") {
-        o.setAttribute("x1", p[0]);
-        o.setAttribute("y1", p[1]);
-        o.setAttribute("x2", p[2]);
-        o.setAttribute("y2", p[3]);
-    }
-    
-    return o;
-};
-
-line(10, 10, 200, 200);
+const setAttributes=(o, ...a)=>{for(const b of a){o.setAttribute(b[0], b[1])}}
+const line = (x1, y1, x2, y2)=>{let l=document.createElementNS("http://www.w3.org/2000/svg","line");;setAttributes(l,["x1",x1],["y1",y1],["x2",x2],["y2",y2]);canvas.appendChild(l);}
