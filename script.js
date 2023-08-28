b10c610c3db8a21c616e114267a19ffbe70b623d
K4RU3 <pzKARUzq2@gmail.com>
2023-08-28 22:19:24 +0900

    test

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const canvas = document.querySelector("#canvas")
-const obj = (name, ...p)=>{return document.createElement(name, name==="line" ? name==="rect" ? {x:p[0],y:p[1],width:p[2],height:p[3]} : {x1:p[0],y1:p[1],x2:p[2],y2:p[3]} : {})}
-const line = (x1,y1,x2,y2)=>{canvas.appendChild()}
-console.log(obj("line", 200, 200, 100,100))
+const canvas = document.querySelector("#canvas");
+
+const line = (x1, y1, x2, y2) => {
+    canvas.appendChild(obj("line", x1, y1, x2, y2));
+};
+
+const obj = (name, ...p) => {
+    let o = document.createElementNS("http://www.w3.org/2000/svg", name);
+    
+    if (name === "line") {
+        o.setAttribute("x1", p[0]);
+        o.setAttribute("y1", p[1]);
+        o.setAttribute("x2", p[2]);
+        o.setAttribute("y2", p[3]);
+    }
+    
+    return o;
+};
+
+line(10, 10, 200, 200);
